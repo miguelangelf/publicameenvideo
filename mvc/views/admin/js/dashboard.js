caaaaa496c8a9f6bf854fdeb4322d6b37c7594a9
Miguel Angel <miguel.angel.flores93@gmail.com>
2015-03-12 21:08:59 -0600

    Mejora  del admin y creacion de archivos para el admin.

--- mvc/views/admin/js/dashboard.js
+++ mvc/views/admin/js/dashboard.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+ 
+  
 });
+
+ for(var i=0;i<100;i++)
+  {
+    $(".counter").text(i);
+      
+  }
+
